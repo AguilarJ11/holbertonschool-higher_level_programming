bc408afac3223e590c983c22ac795f12f618a085
Joaquin <aguilarj4126@gmail.com>
2024-05-09 22:31:40 +0000

    fix?

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,6 +3,7 @@ if __name__ == "__main__":
     from sys import argv
 arg = len(argv)
 total = 0
-for i in range(1, arg + 1):
-    total += int(argv[i])
+if arg > 0:
+    for i in range(1, arg + 1):
+        total += int(argv[i])
 print(total)
