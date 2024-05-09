268a982a3d75ae2503e2d3ea23a39a721881976d
Joaquin <aguilarj4126@gmail.com>
2024-05-09 18:07:43 +0000

    change when argv = 0

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
     if arg == 1:
         print("{} argument:".format(arg))
         print("{}: {}".format(arg, argv[1]))
+    elif arg == 0:
+        print("{} arguments." .format(arg))
     else:
         print("{} arguments:".format(arg))
         for i in range(1, arg + 1):
