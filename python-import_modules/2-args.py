800192654fb6019ae154b5b7f562f8965b3420b4
Joaquin <aguilarj4126@gmail.com>
2024-05-09 17:59:32 +0000

    print fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     from sys import argv
     arg = len(argv) - 1
     if arg == 1:
-        print("{} argument".format(arg))
+        print("{} argument:".format(arg))
         print("{}: {}".format(arg, argv[1]))
     else:
-        print("{} arguments".format(arg))
+        print("{} arguments:".format(arg))
         for i in range(1, arg):
             print("{}: {}".format(i, argv[i]))
