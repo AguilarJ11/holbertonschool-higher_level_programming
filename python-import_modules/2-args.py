8dbab4200f70a7413dff3c2b4d52ddba3b6217d9
Joaquin <aguilarj4126@gmail.com>
2024-05-09 17:56:24 +0000

    permmisons

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    arg = len(sys.argv)
+    from sys import argv
+    arg = len(argv)
     if arg == 1:
         print("{} argument".format(arg))
-        print("{}: {}".format(arg, sys.argv[1]))
+        print("{}: {}".format(arg, argv[1]))
     else:
         print("{} arguments".format(arg))
         for i in range(1, arg):
-            print("{}: {}".format(i, sys.argv[i]))
+            print("{}: {}".format(i, argv[i]))
