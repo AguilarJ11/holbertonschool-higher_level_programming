6a7429d376d6c5175da5f14cdd64ef40a47f9c5d
Joaquin <aguilarj4126@gmail.com>
2024-05-13 19:56:17 +0000

    fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         last = 0
         for i2 in i:
-            if len(i) - 1 == last
+            if len(i) - 1 == last:
                 print("{:d}".format(i2), end="")
             else:
                 print("{:d}".format(i2), end=" ")
