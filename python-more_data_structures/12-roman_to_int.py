8b2f451ee4e3de144f22dea6e5ca3202174b1565
Joaquin <aguilarj4126@gmail.com>
2024-05-14 21:07:36 +0000

    logic fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -10,7 +10,7 @@ def roman_to_int(roman_string):
     num_ant = 0
     for c in roman_string:
         num = roman_numbers[c]
-        if num > num_ant:
+        if num >= num_ant:
             result += num
         elif num < num_ant:
             result -= num
