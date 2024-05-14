36f37940eac29bd91a8eec1874eed7b3b65ac897
Joaquin <aguilarj4126@gmail.com>
2024-05-14 17:23:10 +0000

    exec 2 fix

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    sum = 0
+    suma = 0
     for i in my_list:
-        if my_list.index(i) == -1:
-            sum += i
-    return sum
+        if my_list.count(i) == 1:
+            suma += 1
+    return suma
