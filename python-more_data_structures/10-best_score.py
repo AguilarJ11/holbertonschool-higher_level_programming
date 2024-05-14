7b2c97486a78d150e0a1072fb7879625a950da96
Joaquin <aguilarj4126@gmail.com>
2024-05-14 19:20:47 +0000

    exec 11 done and 10 fix

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
-        return None
-    elif a_dictionary is None:
+    if len(a_dictionary) == 0 or a_dictionary is None:
         return None
     best = ""
     score = 0
