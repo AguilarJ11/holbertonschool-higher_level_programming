94aae13552618a33122150684cd1f6ac374d260c
Joaquin <aguilarj4126@gmail.com>
2024-05-14 21:22:59 +0000

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,13 +4,13 @@ def roman_to_int(roman_string):
     roman_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
                      'D': 500}
     if not isinstance(roman_string, str):
-        return None
+        return 0
     result = 0
     num = 0
     num_ant = 0
     for c in roman_string:
         num = roman_numbers[c]
-        if num <= num_ant or num_ant == 0:
+        if num >= num_ant or num_ant == 0:
             result += num
         elif num > num_ant:
             result -= num_ant
