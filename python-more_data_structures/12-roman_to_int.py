1c06056305e0cb9c293c0a436aa7226f3ba0fded
Joaquin Aguilar <159049117+AguilarJ11@users.noreply.github.com>
2024-05-14 20:49:30 -0300

    logic fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -15,6 +15,6 @@ def roman_to_int(roman_string):
         elif num > num_ant:
             result += num
         else:
-            result -= num
+            result -= num_ant * 2
         num_ant = num
     return result
