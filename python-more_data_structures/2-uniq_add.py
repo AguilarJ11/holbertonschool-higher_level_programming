a44d13b9f04e3a806be5f1003063ec238b4351bc
Joaquin <aguilarj4126@gmail.com>
2024-05-14 19:30:10 +0000

    fix

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -5,7 +5,7 @@ def uniq_add(my_list=[]):
     new_list = my_list[:]
     for i in new_list:
         if len(new_list) == 1:
-            return i
+            return new_list[0]
         if new_list.count(i) > 1:
             new_list.remove(i)
     for i in new_list:
