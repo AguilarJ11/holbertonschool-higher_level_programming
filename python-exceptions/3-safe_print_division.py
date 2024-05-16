b6058e1788de2df919559e7beac2f12dd84a71e9
Joaquin <aguilarj4126@gmail.com>
2024-05-16 19:25:05 +0000

    exce 3:6 done

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     result = 0
     try:
         resutl = a / b
-    except (ValueError, ZeroDivisionError):
+    except Exception:
         return None
     finally:
         print("Inside result: {}".format(result))
