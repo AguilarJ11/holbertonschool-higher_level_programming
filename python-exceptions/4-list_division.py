3efd97a7539420fb6bd14ecdde0b3b6f0ec6527a
Joaquin <aguilarj4126@gmail.com>
2024-05-16 19:44:07 +0000

    fix

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
-    new_list = [list_length]
+    new_list = []
     div = 0
     for i in range(list_lenght)
         try:
