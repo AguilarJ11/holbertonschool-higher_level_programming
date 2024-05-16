89643cdb5a03e7851de810fd9f6cd3a2a20b7fc3
Joaquin <aguilarj4126@gmail.com>
2024-05-16 18:05:27 +0000

    remove except index error

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,10 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     num_print = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            num_print += 1
-        except IndexError:
-            break
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
         except (TypeError, ValueError):
             continue
     print()
