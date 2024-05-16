146ed5e7a31c3f70b12f041cc8d0743e12a6cada
Joaquin <aguilarj4126@gmail.com>
2024-05-16 17:46:51 +0000

    logic fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,9 +6,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             num_print += 1
-        except TypeError:
-            continue
         except IndexError:
             break
+        except (TypeError, ValueError):
+            continue
     print()
     return num_print
