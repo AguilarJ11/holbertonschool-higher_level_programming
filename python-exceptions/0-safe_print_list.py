1b857ba97cdaf0459d46115d8ff95f4b4bab0498
Joaquin <aguilarj4126@gmail.com>
2024-05-16 16:45:06 +0000

    exec 1 n' 2 fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     elem_num = 0
     for i in range(x):
         try:
-            print("{}".format(my_list(i)))
+            print("{}".format(my_list[i]))
             elem_num += 1
         except IndexError:
             break
