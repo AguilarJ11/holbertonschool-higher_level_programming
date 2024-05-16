1a88f7ad6a12cffb56c134113329503a8f2283d8
Joaquin <aguilarj4126@gmail.com>
2024-05-16 17:09:47 +0000

    exec 2 done

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    num_print = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            num_print += 1
+        except (TypeError, ValueError):
+            continue
+    print()
+    return num_print
