aa2db6253c7c1edaa38f447c600cf86182b30a04
Joaquin <aguilarj4126@gmail.com>
2024-05-16 18:06:31 +0000

    remove conditional

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     num_print = 0
     for i in range(x):
         try:
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
         except (TypeError, ValueError):
             continue
     print()
