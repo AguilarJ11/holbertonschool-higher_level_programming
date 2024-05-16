77f5e37c98fe6276879a602163705cfffbf886e6
Joaquin <aguilarj4126@gmail.com>
2024-05-16 16:28:15 +0000

    exec 0 n' 1 done

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    elem_num = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list(i)))
+            elem_num += 1
+        except IndexError:
+            break
+    print()
+    return elem_num
