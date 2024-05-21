da9b9b317ce9e47d19c8ab740ad2af39ae37924b
Joaquin <aguilarj4126@gmail.com>
2024-05-21 17:13:42 +0000

    pycodestyle fix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 This module contains a function to add two integers.
 """
 
+
 def add_integer(a, b=98):
     """
     Adds two integers.
@@ -30,5 +31,5 @@ def add_integer(a, b=98):
         a = int(a)
     elif isinstance(b, float):
         b = int(b)
-    
+
     return a + b
