51f81d0489fac891f59d92f04cfb9d1a963ee97f
Joaquin <aguilarj4126@gmail.com>
2024-05-21 18:09:40 +0000

    documentation add

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,17 +1,40 @@
 #!/usr/bin/python3
+"""
+This module divides all elements of a matrix by a divisor
+"""
+
 
 def matrix_divided(matrix, div):
-    r_lenght = 0
+    """
+    Divides all elements of a matrix by a divisor.
+
+    Args:
+        matrix (list of lists): The matrix to be divided.
+        div (int or float): The divisor.
+
+    Raises:
+        TypeError: If the matrix is not a list of lists containing integers or floats,
+                   or if the divisor is not a number.
+        ZeroDivisionError: If the divisor is zero.
+        TypeError: If each row of the matrix does not have the same size.
+
+    Returns:
+        list of lists: A new matrix with each element divided by the divisor,
+                       rounded to 2 decimal places.
+    """
+    row_length = 0
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
     elif not (isinstance(div, int) or isinstance(div, float)):
         raise TypeError("div must be a number")
+
     for row in matrix:
-        if len(row) != r_lenght and r_lenght!= 0:
+        if len(row) != row_length and row_length != 0:
             raise TypeError("Each row of the matrix must have the same size")
-        r_lenght = len(row)
+        row_length = len(row)
         for i in row:
             if not (isinstance(i, int) or isinstance(i, float)):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-     return list(map(lambda x: round(x / div, 2), matrix))
+    return list(map(lambda x: round(x / div, 2), matrix))
