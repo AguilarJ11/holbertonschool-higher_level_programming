90d06f17d2cb16cd1a72363e2f92a243fd086c8e
Joaquin <aguilarj4126@gmail.com>
2024-05-22 13:58:36 +0000

    documentation add

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""UNITTEST"""
-
+"""
+UNITTEST module for testing max_integer function.
+"""
 
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """ class for unittests """
+    """
+    Class for unit tests of the max_integer function.
+    """
+    
     def test_max_integer(self):
-        """ check possible cases edge cases """
+        """
+        Test different possible cases for the max_integer function,
+        including edge cases.
+        """
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([3]), 3)
         self.assertEqual(max_integer([-3]), -3)
