54519ac6385332639e3334b50e195a34ce011a94
Joaquin <aguilarj4126@gmail.com>
2024-05-23 16:41:30 +0000

    Exercise 0 n 1 done

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+def lookup(obj):
+    """
+    Return the string representation of an object.
+
+    Parameters:
+    obj (object): The object to be represented as a string.
+
+    Returns:
+    str: The string representation of the input object.
+    """
+    return dir(obj)
---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class MyList(list):
+    """Subclass of list with a method to print its elements sorted.
+
+    Args:
+        list: Base class providing a mutable list of objects.
+
+    Methods:
+        print_sorted(): Prints the elements of the list in ascending order.
+    """
+
+    def print_sorted(self):
+        """Prints the elements of the list in ascending order."""
+        print(sorted(self))
