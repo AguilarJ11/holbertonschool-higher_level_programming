3c334cc71d7c62f82b55a3ca0531267ce1cac534
Joaquin <aguilarj4126@gmail.com>
2024-05-24 19:31:31 +0000

    fix area method

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -32,7 +32,7 @@ class Square(Rectangle):
         Returns:
             int: The area of the square.
         """
-        return self.__size * 2
+        return self.__size * self.__size
 
     def __str__(self):
         """
