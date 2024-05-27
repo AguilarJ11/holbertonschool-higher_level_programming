29b69b9b071995814721816da16abb4dcf8eaad5
Joaquin <aguilarj4126@gmail.com>
2024-05-27 20:32:33 +0000

    pycodestyle fix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -14,4 +14,4 @@ def write_file(filename="", text=""):
         text (str): The text to write to the file.
     """
     with open(filename, mode="w", encoding="utf-8") as f:
-       return f.write(text)
+        return f.write(text)
