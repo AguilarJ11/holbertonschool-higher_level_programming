4914e27db61727a245027181e4de7ea75d55b673
Joaquin <aguilarj4126@gmail.com>
2024-05-27 00:15:47 +0000

    Exercise 3 done

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+"""
+A Class that counts the number of items iterated
+"""
+
+
+class CountedIterator:
+    """
+    A Class that counts the number of items iterated over.
+
+    Attributes:
+        iterator (iterator): The internal iterator.
+        iter_count (int): The count of iterated items.
+        iter_len (int): The length of the iterable.
+    """
+
+    def __init__(self, iter_obj):
+        """
+        Initializes with an iterable object.
+
+        Args:
+            iter_obj (iterable): An iterable object.
+        """
+        self.iterator = iter(iter_obj)
+        self.iter_count = 0
+        self.iter_len = len(iter_obj)
+
+    def __next__(self):
+        """
+        Returns the next item and increments the count.
+
+        Raises:
+            StopIteration: If the iterator is exhausted.
+        """
+        if self.iter_count < self.iter_len:
+            next(self.iterator)
+            self.iter_count += 1
+        else:
+            raise StopIteration
+
+    def get_count(self):
+        """
+        Returns the number of items iterated over.
+
+        Returns:
+            int: The count of iterated items.
+        """
+        return self.iter_count
