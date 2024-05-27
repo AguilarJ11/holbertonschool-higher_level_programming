80905fb3b73ef273ee8a56574ea065018deaa54d
Joaquin <aguilarj4126@gmail.com>
2024-05-27 21:10:55 +0000

    Exercise 3 done

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Representation of an objetc in JSON format
+"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """Converts a Python object to a JSON-formatted string.
+
+    Args:
+        my_obj: The Python object to be converted.
+
+    Returns:
+        str: A JSON-formatted string representing the input object.
+    """
+    return json.dumps(my_obj)
