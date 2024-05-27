ab3fa7d78308c7f9be7faccf1935290f71cf0a09
Joaquin <aguilarj4126@gmail.com>
2024-05-27 21:30:47 +0000

    pycodestyle fix

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -7,7 +7,6 @@ Loads a Python object from a JSON file.
 import json
 
 
-
 def load_from_json_file(filename):
     """
     Loads a Python object from a JSON file.
@@ -18,7 +17,8 @@ def load_from_json_file(filename):
     Returns:
         Any: The Python object loaded from the JSON file.
 
-    The function reads the JSON data from the specified file and deserializes it into a Python object.
+    The function reads the JSON data from the specified file
+    and deserializes it into a Python object.
     """
     with open(filename, encoding="utf-8") as f:
         return json.load(f)
