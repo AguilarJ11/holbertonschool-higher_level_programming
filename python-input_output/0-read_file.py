4f12d6e3bb26bd853fae420982e2b436bb4dcaf2
Joaquin <aguilarj4126@gmail.com>
2024-05-27 20:11:34 +0000

    pycodestyle fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -13,7 +13,7 @@ def read_file(filename=""):
     Reads and prints the content of a file.
 
     Args:
-        filename (str): The path to the file to be read. Defaults to an empty string.
+        filename (str): The path to the file to be read.
     """
     with open(filename, encoding="utf-8") as myFile:
         print(myFile.read())
