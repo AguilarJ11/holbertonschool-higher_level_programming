60f20c1d887ff542c3a782818f5c7c934b31abf7
Joaquin <aguilarj4126@gmail.com>
2024-05-28 15:54:17 +0000

    Exercise 7 done

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,3 +5,14 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+arg = sys.argv[1:]
+
+add_arg = []
+
+for i in arg:
+        add_arg.append(i)
+try:
+    add_arg = load_from_json_file("add_item.json")
+    save_to_json_file(add_arg, "add_item.json")
+except:
+    save_to_json_file(add_arg, "add_item.json")
