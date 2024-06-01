c9f720065b2f43bb1964da0d81cb20424d4aa703
Joaquin <aguilarj4126@gmail.com>
2024-06-01 18:41:02 +0000

    testing

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,14 +1,41 @@
 #!/usr/bin/python3
 
-import http.server, socketserver
+import http.server, socketserver, json
 
 class Handler(http.server.BaseHTTPRequestHandler):
     
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write("Hello, this is a simple API!")
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            yeison = json.dump({"name": "John", "age": 30, "city": "New York"})
+            self.wfile.write(yeison)
+
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"Ok")
+
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            yeison = json.dump({"version": "1.0", "description": "A simple API built with http.server"})
+            self.wfile.write(yeison)
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
 
 with socketserver.TCPServer(("", 8000), Handler) as httpd:
     print("serving at port", 8000)
