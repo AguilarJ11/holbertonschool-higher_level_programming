8a942b9d8249284ee09d6ad05483f4aeb739f3fc
Joaquin <aguilarj4126@gmail.com>
2024-06-01 19:57:40 +0000

    Test 4

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -7,7 +7,7 @@ class Handler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
             self.send_response(200)
-            self.send_header("Content-type", "text/plain")
+            self.send_header("Content-type", "text/html")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
 
