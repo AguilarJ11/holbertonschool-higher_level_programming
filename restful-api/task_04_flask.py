c7336e26254c399243cf34dd59041f67213a84c1
Joaquin <aguilarj4126@gmail.com>
2024-06-02 18:11:56 +0000

    test 2

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -5,8 +5,8 @@ import json
 
 app = Flask(__name__)
 
-users = {"jose":{"name":"Jose", "age": 70, "city": "Montevideo"},
-             "pepe":{"name":"pepe", "age": 45, "city": "Bs As"},}
+#users = {"jose":{"name":"Jose", "age": 70, "city": "Montevideo"},
+ #            "pepe":{"name":"pepe", "age": 45, "city": "Bs As"},}
 
 @app.route("/")
 def home():
