1030fb0bf8cbefc70c3b237822e7d8c5160adb46
Joaquin <aguilarj4126@gmail.com>
2024-06-22 20:30:19 +0000

    fix set password

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2
 
 CREATE DATABSE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT ALL PRIVILEGES ON hbtn_0d_2.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
