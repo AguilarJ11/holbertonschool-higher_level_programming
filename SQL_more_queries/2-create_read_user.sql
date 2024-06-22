db75e4be4202a133d95306760a6249808f285a45
Joaquin <aguilarj4126@gmail.com>
2024-06-22 20:43:56 +0000

    Fix for pass the checker...

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,6 @@
 
 CREATE DATABSE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON hbtn_0d_2.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
