b862e0ed94a6a8dac2907a32e1eabec069c14935
Joaquin <aguilarj4126@gmail.com>
2024-06-22 20:36:45 +0000

    1-CREATE_USER FIX

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
 -- give to the user user_0d_1 all privileges on your MySQL server and set a new password
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
