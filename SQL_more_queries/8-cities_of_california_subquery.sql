f1f5fa95323301ae73a12ed379353f43701dbdb1
Joaquin <aguilarj4126@gmail.com>
2024-06-23 13:56:37 -0300

    referenece state_id

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,7 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 
 USE hbtn_0d_usa
-SET @state_california = (SELECT id FROM states WHERE name = 'California');
 SELECT *
 FROM *
-WHERE state_id = @state_california
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
