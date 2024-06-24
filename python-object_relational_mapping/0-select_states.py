025a8d099bacdb09f1b1dd458ccf943d03151892
Joaquin <aguilarj4126@gmail.com>
2024-06-23 21:33:25 -0300

    pycodestyle is back!

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+Write a script that lists all states from the database
+"""
 import MySQLdb
 import sys
 
@@ -7,14 +9,15 @@ import sys
 def get_all_states(username, password, database):
 
     try:
-        db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database)
+        db = MySQLdb.connect(host='localhost', port=3306,
+                             user=username, passwd=password, db=database)
         cur = db.cursor()
-        cur.execute("SELECT * FROM states ORDER BY ID")
+        cur.execute("SELECT * FROM states ORDER BY states.id")
         rows = cur.fetchall()
         for row in rows:
             print(row)
     except MySQLdb.Error as e:
-        print (e)
+        print(e)
 
     cur.close()
     db.close()
@@ -25,9 +28,8 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Please enter only username, passowrd and database")
         sys.exit(1)
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
     get_all_states(username, password, database)
