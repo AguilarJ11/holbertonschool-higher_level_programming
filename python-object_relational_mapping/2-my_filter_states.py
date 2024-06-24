16119faabffcfd636b6257c75ac3cd19ef19d145
Joaquin <aguilarj4126@gmail.com>
2024-06-24 14:01:19 -0300

    add docstring

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,6 +8,9 @@ import sys
 
 
 def get_all_states():
+    """
+    commentario
+    """
     try:
         db = MySQLdb.connect(host='localhost',
                              port=3306,
@@ -15,12 +18,12 @@ def get_all_states():
                              passwd=sys.argv[2],
                              db=sys.argv[3])
         cur = db.cursor()
-        arg = sys.argv[4]
+        name = sys.argv[4]
         query = 'SELECT * \
                     FROM states \
                     WHERE name = %s \
                     ORDER BY id ASC'
-        cur.execute(query, (arg,))
+        cur.execute(query, (name,))
         rows = cur.fetchall()
         for row in rows:
             print(row)
@@ -32,4 +35,7 @@ def get_all_states():
 
 
 if __name__ == '__main__':
+    """
+    comentario interesante
+    """
     get_all_states()
