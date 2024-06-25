e4846338b443a188639410c67a16ed73916ed49e
Joaquin <aguilarj4126@gmail.com>
2024-06-24 23:42:46 -0300

    exercise 12 done

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+
+
+if __name__ == "__main__":
+
+    engine = create_engine(
+        f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}",
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.id == 2)
+    states = states.update({State.name: "New Mexico"})
+    session.commit()
+    session.close()
