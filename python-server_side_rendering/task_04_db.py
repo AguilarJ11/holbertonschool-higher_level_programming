943978f26d5300055e0b946a6f594f473aa65b20
Joaquin <aguilarj4126@gmail.com>
2024-07-19 16:35:56 -0300

    uploading progress, exercise unfinished

--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from flask import Flask, render_template, request
-import json, csv
+import json, csv, sqlite3
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -13,10 +13,10 @@ class Products(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    category = db.Column(db.String(50), nullable=False)
+    category = db.Column(db.String(50), nullable=False) 
     price = db.Column(db.Float(50), nullable=False)
 
-def create_database():
+def create_database_alchemy():
     with app.app_context():
         db.create_all()
     new_product_one = Products(name="Laptop", category="Electronics", price=799.99)
@@ -24,6 +24,26 @@ def create_database():
     db.session.add(new_product_one)
     db.session.add(new_product_two)
     db.session.commit()
+
+def create_database_sqlite3():
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Products (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            category TEXT NOT NULL,
+            price REAL NOT NULL
+        )
+    ''')
+    cursor.execute('''
+        INSERT INTO Products (id, name, category, price)
+        VALUES
+        (1, 'Laptop', 'Electronics', 799.99),
+        (2, 'Coffee Mug', 'Home Goods', 15.99)
+    ''')
+    conn.commit()
+    conn.close()
 
 """
 Endpoints
@@ -68,6 +88,7 @@ def products():
                     bandera = True
             if not bandera:
                 data = 1
+
     elif persistence_type == "csv":
         with open ("products.csv", newline="") as csv_file:
             data = csv.DictReader(csv_file)
@@ -81,8 +102,9 @@ def products():
                     bandera = True
             if not bandera:
                 data = 1
-    elif persistence_type == "sql":
-        create_database()
+
+    elif persistence_type == "sql_alchemy":
+        create_database_alchemy()
         if id:
             try:
                 data_query = db.session.get(Products, id)
@@ -95,6 +117,19 @@ def products():
             data = []
             for products in data_query:
                 data.append(products)
+
+    elif persistence_type == "sql":
+        if id:
+            create_database_sqlite3()
+            conn = sqlite3.connect('products.db')
+            cursor = conn.cursor()
+            cursor.execute("SELECT * from Products")
+            data_query = cursor.fetchall()
+            keys_list = []
+            for keys in cursor.description:
+                keys_list.append(keys[0])
+
+  
     else:
         data = []
     return render_template('product_display.html', products=data)
