f05c49526a09862e66899ac78393564763f40112
Joaquin <aguilarj4126@gmail.com>
2024-07-19 00:06:16 -0300

    the solution

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -36,19 +36,27 @@ def products():
         with open("products.json", "r") as json_file:
             data = json.load(json_file)
         if id:
+            bandera = False
             for dictionary in data:
                 if str(dictionary["id"]) == id:
                     data = []
                     data.append(dictionary)
+                    bandera = True
+            if not bandera:
+                data = 1
     elif file_type == "csv":
         with open ("products.csv", newline="") as csv_file:
             data = csv.DictReader(csv_file)
             data = list(data)
         if id:
+            bandera = False
             for row in data:
                 if str(row["id"]) == id:
                     data = []
                     data.append(row)
+                    bandera = True
+            if not bandera:
+                data = 1
     else:
         data = []
     return render_template('product_display.html', products=data)
